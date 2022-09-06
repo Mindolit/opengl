260d00d941bbd5eb314a45fd157063df0d0fc992
MINDOLIT <kms5486@hanmail.net>
2022-09-06 23:02:28 +0900

    Shader.h추가

--- baseopengl/Project1/main.cpp
+++ baseopengl/Project1/main.cpp
@@ -2,14 +2,25 @@
 #include<GLFW/glfw3.h>
 #include<iostream>
 
+//사용자정의 Header
+#include"SHADER.h"
+
 
 void framebuffer_size_callback(GLFWwindow* window, int widht, int height);
+
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+};
+
 
 int main()
 {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	GLFWwindow* window = glfwCreateWindow(800, 600, "MINDOL", NULL, NULL);
 	if (window == NULL)
 	{
@@ -18,6 +29,7 @@ int main()
 	}
 
 	//callback 함수를 통해 window의 viewport 설정
+	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	//GLAD설정
@@ -28,11 +40,27 @@ int main()
 	}
 
 
+	unsigned int VAO, VBO;
+	glGenVertexArrays(1,&VAO);
+	glBindVertexArray(VAO);
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	//pointer 지정
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	Shader TraiangleShader("Triangle.vert", "Triangle.frag");
+
+
+
 	//렌더 루프
 	while (!glfwWindowShouldClose(window))
 	{
-
-
+		TraiangleShader.use();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
