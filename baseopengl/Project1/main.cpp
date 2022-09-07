da13d7719d0074d8ec158163d4280a4d841e867a
MINDOLIT <kms5486@hanmail.net>
2022-09-07 16:26:34 +0900

    Exrecises 1 solutuion

--- baseopengl/Project1/main.cpp
+++ baseopengl/Project1/main.cpp
@@ -10,9 +10,9 @@ void framebuffer_size_callback(GLFWwindow* window, int widht, int height);
 void processInput(GLFWwindow* window);
 float vertices[] = {
 	// positions         // colors
-	  0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-	 -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-	  0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+	  -0.5f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+	 0.5f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+	  0.0f,  -0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
 	  // x ,y,z  , r,g,b
 };
 
