a239762c92e55208055bee5df5dc48c17803d008
MINDOLIT <kms5486@hanmail.net>
2022-09-08 00:08:05 +0900

    Camera LookAt matrix implementation

--- baseopengl/Project1/main.cpp
+++ baseopengl/Project1/main.cpp
@@ -185,8 +185,16 @@ int main()
 		//TriangleShader.setFloat4("unicolor", 0.0f, greenValue, 0.0f, 1.0f);
 		
 
+		const float radius = 10.0f;
+		float camX = sin(glfwGetTime()) * radius;
+		float camZ = cos(glfwGetTime()) * radius;
 		glm::mat4 view;
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));
+		view = glm::lookAt(glm::vec3(camX, 0.0, camZ),
+			glm::vec3(0, 0, 0),
+			glm::vec3(0.0, 1.0, 0.0)
+		);
+
+
 
 		glm::mat4 projection;
 		projection = glm::perspective(glm::radians(45.0f), screenWidth / screenHeight, 0.1f, 100.0f);
